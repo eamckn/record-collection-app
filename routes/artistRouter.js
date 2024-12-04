8c9ed41b7b92ea4069672b608ef90eb1ddf5adce
Eamon McKeon <154269556+eamckn@users.noreply.github.com>
2024-12-04 12:00:57 -0500

    feat: add new routes for views

--- routes/artistRouter.js
+++ routes/artistRouter.js
@@ -4,5 +4,8 @@ const artistController = require("../controllers/artistController");
 
 artistRouter.get("/", artistController.displayArtistRecordsGet);
 artistRouter.get("/title", artistController.displayRecordDetailsGet);
+artistRouter.get("/new", artistController.displayNewRecordFormGet);
+artistRouter.get("/update", artistController.updateArtistGet);
+artistRouter.get("/title/update", artistController.updateRecordGet);
 
 module.exports = { artistRouter };
