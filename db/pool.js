2eefc64b374924839bc6944fa69d72f7fc809e5b
Eamon McKeon <154269556+eamckn@users.noreply.github.com>
2024-12-18 19:29:31 -0500

    feat: make pool connectionString an env var

--- db/pool.js
+++ db/pool.js
@@ -1,5 +1,5 @@
 const { Pool } = require("pg");
 
 module.exports = new Pool({
-  connectionString: "postgresql://Eamon:1108madison@localhost:5432/top_records",
+  connectionString: process.env.DATABASE_PUBLIC_URL,
 });
