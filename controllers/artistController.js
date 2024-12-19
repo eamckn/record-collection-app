30aaaad98f9a9fadabc9b78190b2f236995cc583
Eamon McKeon <154269556+eamckn@users.noreply.github.com>
2024-12-19 10:47:03 -0500

    feat: render artist names in sidebar for all views

--- controllers/artistController.js
+++ controllers/artistController.js
@@ -4,31 +4,40 @@ const displayArtistRecordsGet = async (req, res) => {
   const { artist } = req.params;
   //console.log("clicked artist is " + artist);
   const records = await db.getArtistRecords(artist);
+  const artists = await db.getAllArtists();
   //records.forEach((record) => console.log(record));
-  res.render("category", { artist: artist, records: records });
+  res.render("category", {
+    artist: artist,
+    records: records,
+    artists: artists,
+  });
 };
 
-const displayNewRecordFormGet = (req, res) => {
+const displayNewRecordFormGet = async (req, res) => {
   const { artist } = req.params;
-  res.render("newRecord", { artist: artist });
+  const artists = await db.getAllArtists();
+  res.render("newRecord", { artist: artist, artists: artists });
 };
 
 const updateRecordGet = async (req, res) => {
   const { id } = req.params;
   const [record] = await db.getRecordDetails(id);
+  const artists = await db.getAllArtists();
   //console.log(record);
-  res.render("updateRecord", { record: record });
+  res.render("updateRecord", { record: record, artists: artists });
 };
 
-const updateArtistGet = (req, res) => {
+const updateArtistGet = async (req, res) => {
   const { artist } = req.params;
-  res.render("updateCategory", { artist: artist });
+  const artists = await db.getAllArtists();
+  res.render("updateCategory", { artist: artist, artists: artists });
 };
 
 const displayRecordDetailsGet = async (req, res) => {
   const { id } = req.params;
   const [record] = await db.getRecordDetails(id);
-  res.render("details", { record: record });
+  const artists = await db.getAllArtists();
+  res.render("details", { record: record, artists: artists });
 };
 
 module.exports = {
