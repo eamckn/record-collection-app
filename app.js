a066eb88c35c6d7c91d21eddb0aba0d02c93499a
Eamon McKeon <154269556+eamckn@users.noreply.github.com>
2024-12-23 20:53:04 -0500

    feat: serve css assests via express

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ const path = require("node:path");
 const { indexRouter } = require("./routes/indexRouter");
 const { artistRouter } = require("./routes/artistRouter");
 const PORT = process.env.PORT || 8080;
+const assetsPath = path.join(__dirname, "styles");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(assetsPath));
 
 app.use("/", indexRouter);
 app.use("/artists", artistRouter);
