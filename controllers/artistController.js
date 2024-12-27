fc5e0ab1630f401f9748f27e43ae84d611288aec
Eamon McKeon <154269556+eamckn@users.noreply.github.com>
2024-12-27 15:13:41 -0500

    feat: add basic form validation

--- controllers/artistController.js
+++ controllers/artistController.js
@@ -1,4 +1,47 @@
 const db = require("../db/queries");
+const { body, validationResult } = require("express-validator");
+
+const blankMsg = "cannot be left blank";
+
+const validateArtist = [
+  body("artist_name")
+    .trim()
+    .notEmpty()
+    .withMessage("Artist name " + blankMsg),
+  body("artist_img")
+    .trim()
+    .notEmpty()
+    .withMessage("This field " + blankMsg)
+    .isURL()
+    .withMessage("Please enter a valid URL for the artist image"),
+];
+
+const validateRecord = [
+  body("record_title")
+    .trim()
+    .notEmpty()
+    .withMessage("Record title " + blankMsg),
+  body("record_year")
+    .trim()
+    .notEmpty()
+    .withMessage("Release year " + blankMsg)
+    .isInt()
+    .withMessage("Release year must be an integer"),
+  body("record_label")
+    .trim()
+    .notEmpty()
+    .withMessage("Label field " + blankMsg),
+  body("record_genre")
+    .trim()
+    .notEmpty()
+    .withMessage("Genre " + blankMsg),
+  body("record_art")
+    .trim()
+    .notEmpty()
+    .withMessage("This field " + blankMsg)
+    .isURL()
+    .withMessage("Please enter a valid URL for the artist image"),
+];
 
 const displayArtistRecordsGet = async (req, res) => {
   const { artist_id } = req.params;
@@ -26,25 +69,38 @@ const displayNewRecordFormGet = async (req, res) => {
   res.render("newRecord", { artist: artist, artists: artists });
 };
 
-const addNewRecordPost = async (req, res) => {
-  const {
-    record_title,
-    record_artist_id,
-    record_year,
-    record_genre,
-    record_label,
-    record_art,
-  } = req.body;
-  await db.addNewRecord(
-    record_title,
-    record_artist_id,
-    record_year,
-    record_genre,
-    record_label,
-    record_art
-  );
-  res.redirect(`/artists/${record_artist_id}`);
-};
+const addNewRecordPost = [
+  validateRecord,
+  async (req, res) => {
+    const {
+      record_title,
+      record_artist_id,
+      record_year,
+      record_genre,
+      record_label,
+      record_art,
+    } = req.body;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const artists = await db.getAllArtists();
+      const artist = await db.getArtistByArtistID(record_artist_id);
+      return res.status(400).render("newRecord", {
+        artist: artist,
+        artists: artists,
+        errors: errors.array(),
+      });
+    }
+    await db.addNewRecord(
+      record_title,
+      record_artist_id,
+      record_year,
+      record_genre,
+      record_label,
+      record_art
+    );
+    res.redirect(`/artists/${record_artist_id}`);
+  },
+];
 
 const updateRecordGet = async (req, res) => {
   const { record_id } = req.params;
@@ -53,26 +109,39 @@ const updateRecordGet = async (req, res) => {
   res.render("updateRecord", { record: record, artists: artists });
 };
 
-const updateRecordPost = async (req, res) => {
-  const {
-    update_title,
-    update_release_year,
-    update_genre,
-    update_label,
-    update_art,
-    update_id,
-  } = req.body;
-  await db.updateRecord(
-    update_title,
-    update_release_year,
-    update_genre,
-    update_label,
-    update_art,
-    update_id
-  );
-  const artist_id = await db.getArtistIdFromRecordID(update_id);
-  res.redirect(`/artists/${artist_id}/${update_id}`);
-};
+const updateRecordPost = [
+  validateRecord,
+  async (req, res) => {
+    const {
+      record_title,
+      record_year,
+      record_label,
+      record_genre,
+      record_art,
+      record_id,
+    } = req.body;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const record = await db.getRecordDetails(record_id);
+      const artists = await db.getAllArtists();
+      return res.status(400).render("updateRecord", {
+        record: record,
+        artists: artists,
+        errors: errors.array(),
+      });
+    }
+    await db.updateRecord(
+      record_title,
+      record_year,
+      record_genre,
+      record_label,
+      record_art,
+      record_id
+    );
+    const artist_id = await db.getArtistIdFromRecordID(record_id);
+    res.redirect(`/artists/${artist_id}/${record_id}`);
+  },
+];
 
 const updateArtistGet = async (req, res) => {
   const { artist_id } = req.params;
@@ -81,11 +150,24 @@ const updateArtistGet = async (req, res) => {
   res.render("updateCategory", { artist: artist, artists: artists });
 };
 
-const updateArtistPost = async (req, res) => {
-  const { update_artist, update_artist_img, update_artist_id } = req.body;
-  await db.updateArtist(update_artist, update_artist_img, update_artist_id);
-  res.redirect(`/artists/${update_artist_id}`);
-};
+const updateArtistPost = [
+  validateArtist,
+  async (req, res) => {
+    const errors = validationResult(req);
+    const { artist_name, artist_img, artist_id } = req.body;
+    if (!errors.isEmpty()) {
+      const artists = await db.getAllArtists();
+      const artist = await db.getArtistByArtistID(artist_id);
+      return res.status(400).render("updateCategory", {
+        artist: artist,
+        artists: artists,
+        errors: errors.array(),
+      });
+    }
+    await db.updateArtist(artist_name, artist_img, artist_id);
+    res.redirect(`/artists/${artist_id}`);
+  },
+];
 
 const deleteArtistRecordPost = async (req, res) => {
   const { record_id } = req.body;
@@ -101,6 +183,7 @@ const deleteArtistPost = async (req, res) => {
 };
 
 module.exports = {
+  validateArtist,
   displayArtistRecordsGet,
   displayRecordDetailsGet,
   displayNewRecordFormGet,
--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,4 +1,6 @@
 const db = require("../db/queries");
+const { validationResult } = require("express-validator");
+const { validateArtist } = require("./artistController");
 
 const displayHomeGet = async (req, res) => {
   const records = await db.getAllRecords();
@@ -11,11 +13,22 @@ const addNewArtistGet = async (req, res) => {
   res.render("newCategory", { artists: artists });
 };
 
-const addNewArtistPost = async (req, res) => {
-  const { new_artist, new_artist_img } = req.body;
-  await db.addNewArtist(new_artist, new_artist_img);
-  res.redirect("/");
-};
+const addNewArtistPost = [
+  validateArtist,
+  async (req, res) => {
+    const { artist_name, artist_img } = req.body;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const artists = await db.getAllArtists();
+      return res.status(400).render("newCategory", {
+        artists: artists,
+        errors: errors.array(),
+      });
+    }
+    await db.addNewArtist(artist_name, artist_img);
+    res.redirect("/");
+  },
+];
 
 module.exports = {
   displayHomeGet,
